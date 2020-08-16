ad75bdb312f558f0bfdee876d396e8da5dbf8a74
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-16 21:11:49 +0900

    [기초-조건/선택실행구조] 정수 3개 입력받아 짝/홀 출력하기(설명)

---
+++ 1000/1066.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1, num2, num3;
+	 scanf("%d %d %d",&num1,&num2,&num3);
+ 
+	if(0==num1%2)
+		printf("even\n");
+		else
+			printf("odd\n");
+	
+ 	if(0==num2%2)  
+		printf("even\n");
+		else
+			printf("odd\n");
+	 
+  	if(0==num3%2)  
+		printf("even\n");
+		else
+			printf("odd\n");
+	 
+	
+	return 0;
+}
+
