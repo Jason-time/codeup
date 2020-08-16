4bb3e49a2f50a8a854ef96d7384f52babdcc8b44
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-16 22:21:11 +0900

    [기초-종합] 수 나열하기1

---
+++ 1000/1089.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int w,h,b,sum,i;
+	long long int to;
+	double re;
+	
+	scanf("%d %d %d", &w,&h,&b);  
+	sum =w;
+	for(i=0; i< b-1; i++){
+		sum +=h;
+		
+		 
+	}
+		printf("%d ",sum);
+	  
+	return 0;
+}
