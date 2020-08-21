2bb2f280cd7227a7125bb66ec2ac9e113e83ed2a
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-21 23:28:52 +0900

    1504 : 지그재그 배열 2 해결

---
+++ 1500/1504.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ #pragma warning(disable:4996)
+
+int main(void) {
+
+	int num=0,i = 0, j = 0, nInput = 0, nCounter = 0,nFlag=1;
+	int array[101][101] = { 0 };
+	scanf("%d", &nInput);
+
+	for (i = 0;i < nInput;i++) {
+		if (nFlag) {
+			for (j = 0;j < nInput;++j) {
+				array[j][i] = ++nCounter;
+				nFlag = 0;
+			}
+		}
+		else {
+			
+		for (j = 0;j < nInput;j++)
+			array[nInput-1-j][i] = ++nCounter;
+		nFlag = 1;
+		}
+	}
+
+	for (i = 0;i < nInput;i++)
+	{
+		for (j = 0;j < nInput;j++) {
+			printf("%d ", array[i][j]);
+		}
+			putchar('\n');
+	}
+	return 0;
+}
